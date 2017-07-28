4cbfe752b37c2a06324f555acb4ddcd6c019307e
Mayurgupta3 <mgupta7042@gmail.com>
2017-07-28 18:52:06 +0530

    project with search objective

--- Instaclone/Instaclone/urls.py
+++ Instaclone/Instaclone/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 
+from myapp.views import signup_view, login_view, feed_view, post_view, like_view, comment_view, search_view, commentlike_view
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url('commentlike', commentlike_view),
+    url('search/', search_view),
+    url('like/', like_view),
+    url('post/', post_view),
+    url('feed/', feed_view),
+    url('comment/', comment_view),
+    url('login/', login_view),
+    url('', signup_view)
 ]
